a35d764d10a12b0dd796a32b97345e564c54ab20
Kristina Peru <krissy.germanov@gmail.com>
2022-03-23 13:16:50 -0600

    Creates company aggregate and association methods

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,12 @@
 class Company < ActiveRecord::Base
     has_many :freebies
     has_many :devs, through: :freebies
+    
+        def self.oldest_company
+            self.all.min_by { |c| c.founding_year }
+        end
 
+    def give_freebie(dev, item_name, value)
+        self.freebies.create(dev: dev, item_name: item_name, value: value)
+    end
 end
