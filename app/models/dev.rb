574ddf0677911978d43579ec5e78905d628989a4
Kristina Peru <krissy.germanov@gmail.com>
2022-03-23 16:32:20 -0600

    completes all

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -5,10 +5,18 @@ class Dev < ActiveRecord::Base
     def recieved_one?(item_name)
         item = self.freebies.find_by(item_name: item_name)
         # item ? true : false
+        self.freebies.pluck(item_names).include?(item.name)
     end
 
     def give_away(dev, freebie)
-        
+        # if self.recieved_one?(freebie.item_name)
+        #     freebie.update(dev: dev)
+        #     dev.reload
+        #     self.reload
+        # else
+        #     "You can't give away something you don't have!"
+        # end
+        freebie.update(dev: dev) unless freebie.dev != self
     end
 
 end
