b8c11a81a3af63cd57f011688561ba4d1cff2923
Kristina Peru <krissy.germanov@gmail.com>
2022-03-23 13:53:22 -0600

    works on dev class

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -2,4 +2,13 @@ class Dev < ActiveRecord::Base
     has_many :freebies
     has_many :companies, through: :freebies
 
+    def recieved_one?(item_name)
+        item = self.freebies.find_by(item_name: item_name)
+        # item ? true : false
+    end
+
+    def give_away(dev, freebie)
+        
+    end
+
 end
